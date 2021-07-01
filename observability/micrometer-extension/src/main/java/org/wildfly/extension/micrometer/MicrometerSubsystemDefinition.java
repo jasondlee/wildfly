65db6eeddee93ed19b0ae811c30484828c06dc3f
Jason Lee <jason@steeplesoft.com>
2021-07-01 14:20:46 -0500

    [WFLY-14947] Add Micrometer support
    Initial extension scaffolding

---
+++ observability/micrometer-extension/src/main/java/org/wildfly/extension/micrometer/MicrometerSubsystemDefinition.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *
+ * Copyright 2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.extension.micrometer;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.jboss.as.controller.AttributeDefinition;
+import org.jboss.as.controller.PersistentResourceDefinition;
+import org.jboss.as.controller.SimpleResourceDefinition;
+
+public class MicrometerSubsystemDefinition extends PersistentResourceDefinition {
+    public static final String[] MODULES = {
+    };
+
+    public static final String[] EXPORTED_MODULES = {
+            "org.wildfly.extension.micrometer-extension"
+    };
+
+    static final AttributeDefinition[] ATTRIBUTES = {
+    };
+
+    public static final MicrometerSubsystemDefinition INSTANCE = new MicrometerSubsystemDefinition();
+
+    protected MicrometerSubsystemDefinition() {
+
+        super(new SimpleResourceDefinition.Parameters(MicrometerSubsystemExtension.SUBSYSTEM_PATH,
+                MicrometerSubsystemExtension.getResourceDescriptionResolver(MicrometerSubsystemExtension.SUBSYSTEM_NAME))
+                .setAddHandler(MicrometerSubsystemAdd.INSTANCE)
+                .setRemoveHandler(MicrometerSubsystemRemove.INSTANCE));
+
+    }
+
+    @Override
+    public Collection<AttributeDefinition> getAttributes() {
+        return Arrays.asList(ATTRIBUTES);
+    }
+}
