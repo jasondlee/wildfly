290a6cb07ebd4d62f2b5399b35ec47ffb7f2570e
Paul Ferraro <paul.ferraro@redhat.com>
2023-03-16 21:21:37 +0100

    Leverage PersistentSubsystemSchema and SubsystemResourceDescriptionResolver (#11)
    * Leverage PersistentSubsystemSchema.
    
    * Leverage SubsystemResourceDescriptionResolver.

--- microprofile/microprofile-telemetry/src/main/java/org/wildfly/extension/microprofile/telemetry/MicroProfileTelemetryExtension.java
+++ microprofile/microprofile-telemetry/src/main/java/org/wildfly/extension/microprofile/telemetry/MicroProfileTelemetryExtension.java
@@ -22,17 +22,23 @@ package org.wildfly.extension.microprofile.telemetry;
 import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUBSYSTEM;
 
 import java.util.EnumSet;
+import java.util.List;
 
 import org.jboss.as.controller.Extension;
 import org.jboss.as.controller.ExtensionContext;
 import org.jboss.as.controller.ModelVersion;
 import org.jboss.as.controller.PathElement;
+import org.jboss.as.controller.PersistentResourceXMLDescription;
+import org.jboss.as.controller.PersistentResourceXMLDescriptionReader;
+import org.jboss.as.controller.PersistentResourceXMLDescriptionWriter;
 import org.jboss.as.controller.SubsystemRegistration;
-import org.jboss.as.controller.descriptions.ResourceDescriptionResolver;
-import org.jboss.as.controller.descriptions.StandardResourceDescriptionResolver;
+import org.jboss.as.controller.descriptions.ParentResourceDescriptionResolver;
+import org.jboss.as.controller.descriptions.SubsystemResourceDescriptionResolver;
 import org.jboss.as.controller.operations.common.GenericSubsystemDescribeHandler;
 import org.jboss.as.controller.parsing.ExtensionParsingContext;
 import org.jboss.as.controller.registry.ManagementResourceRegistration;
+import org.jboss.dmr.ModelNode;
+import org.jboss.staxmapper.XMLElementReader;
 
 public class MicroProfileTelemetryExtension implements Extension {
     /**
@@ -41,26 +47,17 @@ public class MicroProfileTelemetryExtension implements Extension {
     public static final String SUBSYSTEM_NAME = "microprofile-telemetry";
 
     static final PathElement SUBSYSTEM_PATH = PathElement.pathElement(SUBSYSTEM, SUBSYSTEM_NAME);
-    private static final String RESOURCE_NAME = MicroProfileTelemetryExtension.class.getPackage().getName() + ".LocalDescriptions";
+    static final ParentResourceDescriptionResolver SUBSYSTEM_RESOLVER = new SubsystemResourceDescriptionResolver(SUBSYSTEM_NAME, MicroProfileTelemetryExtension.class);
+
     private static final ModelVersion VERSION_1_0_0 = ModelVersion.create(1, 0, 0);
     private static final ModelVersion CURRENT_MODEL_VERSION = VERSION_1_0_0;
 
-    static ResourceDescriptionResolver getResourceDescriptionResolver(final boolean useUnprefixedChildTypes,
-                                                                      final String... keyPrefix) {
-        StringBuilder prefix = new StringBuilder();
-        for (String kp : keyPrefix) {
-            if (prefix.length() > 0){
-                prefix.append('.');
-            }
-            prefix.append(kp);
-        }
-        return new StandardResourceDescriptionResolver(prefix.toString(), RESOURCE_NAME,
-                MicroProfileTelemetryExtension.class.getClassLoader(), true, useUnprefixedChildTypes);
-    }
+    private final PersistentResourceXMLDescription currentDescription = MicroProfileTelemetrySubsystemSchema.CURRENT.getXMLDescription();
+
     @Override
     public void initialize(ExtensionContext context) {
         final SubsystemRegistration subsystem = context.registerSubsystem(SUBSYSTEM_NAME, CURRENT_MODEL_VERSION);
-        subsystem.registerXMLElementWriter(new MicroProfileTelemetryParser(MicroProfileTelemetrySchema.CURRENT));
+        subsystem.registerXMLElementWriter(new PersistentResourceXMLDescriptionWriter(this.currentDescription));
 
         final ManagementResourceRegistration registration =
                 subsystem.registerSubsystemModel(new MicroProfileTelemetrySubsystemDefinition());
@@ -70,8 +67,9 @@ public class MicroProfileTelemetryExtension implements Extension {
 
     @Override
     public void initializeParsers(ExtensionParsingContext context) {
-        for (MicroProfileTelemetrySchema schema : EnumSet.allOf(MicroProfileTelemetrySchema.class)) {
-            context.setSubsystemXmlMapping(SUBSYSTEM_NAME, schema.getNamespaceUri(), new MicroProfileTelemetryParser(schema));
+        for (MicroProfileTelemetrySubsystemSchema schema : EnumSet.allOf(MicroProfileTelemetrySubsystemSchema.class)) {
+            XMLElementReader<List<ModelNode>> reader = (schema == MicroProfileTelemetrySubsystemSchema.CURRENT) ? new PersistentResourceXMLDescriptionReader(this.currentDescription) : schema;
+            context.setSubsystemXmlMapping(SUBSYSTEM_NAME, schema.getNamespace().getUri(), reader);
         }
     }
 }
