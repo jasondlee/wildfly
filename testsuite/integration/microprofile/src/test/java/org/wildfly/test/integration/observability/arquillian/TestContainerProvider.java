69162d8d5aff129100cff3720f81ce7af2df6fa1
Jason Lee <jason@steeplesoft.com>
2024-06-27 11:21:49 -0500

    Use the precedence to ensure the TestContainer is started before deployment starts. (#16)
    Signed-off-by: James R. Perkins <jperkins@redhat.com>
    Co-authored-by: James R. Perkins <jperkins@redhat.com>

--- testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/observability/arquillian/TestContainerProvider.java
+++ testsuite/integration/microprofile/src/test/java/org/wildfly/test/integration/observability/arquillian/TestContainerProvider.java
@@ -5,30 +5,22 @@
 package org.wildfly.test.integration.observability.arquillian;
 
 import java.lang.annotation.Annotation;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.jboss.arquillian.core.api.Instance;
 import org.jboss.arquillian.core.api.annotation.Inject;
 import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.arquillian.test.spi.annotation.ClassScoped;
 import org.jboss.as.arquillian.container.AbstractTargetsContainerProvider;
 import org.testcontainers.containers.GenericContainer;
 
 public class TestContainerProvider extends AbstractTargetsContainerProvider {
     @Inject
-    private Instance<AtomicReference<GenericContainer<?>>> genericContainerInstance;
+    @ClassScoped
+    private Instance<GenericContainer<?>> genericContainerInstance;
 
     @Override
     public Object doLookup(ArquillianResource resource, Annotation... qualifiers) {
-        AtomicReference<GenericContainer<?>> reference = genericContainerInstance.get();
-        if (reference != null) {
-            GenericContainer<?> container = reference.get();
-            if (container != null && !container.isRunning()) {
-                container.start();
-            }
-            return container;
-        } else {
-            return null;
-        }
+        return genericContainerInstance.get();
     }
 
     @Override
